082ca794b34bdb8eddb3b8a59232feb56233a682
Matthew Mahoney <matthew.a.mahoney@gmail.com>
2026-04-09 10:47:21 +0000

    Update app pages

--- src/api/backendFunctions.js
+++ src/api/backendFunctions.js
@@ -2,10 +2,12 @@ import { base44 } from './base44Client';
 
 export const processDhlBatch = base44.functions.processDhlBatch;
 export const processDhlOneDrive = base44.functions.processDhlOneDrive;
-export const backfillAddresses = base44.functions.backfillAddresses;
 export const saveDhlRecord = base44.functions.saveDhlRecord;
 export const processDhlPdfsWithOcr = base44.functions.processDhlPdfsWithOcr;
 export const saveDhlTickets = base44.functions.saveDhlTickets;
 export const processDhlMonitor = base44.functions.processDhlMonitor;
 export const saveDhlMonitorBatch = base44.functions.saveDhlMonitorBatch;
 export const saveDhlRecordsFromMonitor = base44.functions.saveDhlRecordsFromMonitor;
+export const backfillAddresses = base44.functions.backfillAddresses;
+export const saveDhlBatchFromMonitor = base44.functions.saveDhlBatchFromMonitor;
+export const saveDhlBatch = base44.functions.saveDhlBatch;

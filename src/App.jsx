e79e2d1fe9de9d687734c8a17e65ba224b468a8a
Matthew Mahoney <matthew.a.mahoney@gmail.com>
2026-04-11 08:11:29 +0000

    File changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
+import { useEffect } from 'react'
 import { Toaster } from '@/components/ui/toaster'
 import { QueryClientProvider } from '@tanstack/react-query'
 import { queryClientInstance } from '@/lib/query-client'
@@ -38,11 +39,16 @@ const AuthenticatedApp = () => {
     );
   }
 
+  useEffect(() => {
+    if (!isPublic && authError?.type === 'auth_required') {
+      navigateToLogin();
+    }
+  }, [authError, isPublic]);
+
   if (!isPublic && authError) {
     if (authError.type === 'user_not_registered') {
       return <UserNotRegisteredError />;
     } else if (authError.type === 'auth_required') {
-      navigateToLogin();
       return null;
     }
   }
